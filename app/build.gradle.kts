b93cee29a752fe95353b45c9a5e78d6fbed86f81
mu54omd <mu54omd@vivaldi.net>
2024-10-15 13:12:27 +0330

    add keep_alive input parameter for ollama

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.ollamaui"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
