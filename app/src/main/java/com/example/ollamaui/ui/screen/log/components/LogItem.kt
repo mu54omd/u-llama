5d33fa57f8853f3f79dfd30bb3d1f3313f6f4971
mu54omd <mu54omd@vivaldi.net>
2025-04-01 14:44:23 +0330

    + change Log Item UI

--- app/src/main/java/com/example/ollamaui/ui/screen/log/components/LogItem.kt
+++ app/src/main/java/com/example/ollamaui/ui/screen/log/components/LogItem.kt
@@ -2,18 +2,25 @@ package com.example.ollamaui.ui.screen.log.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ollamaui.ui.theme.OllamaUITheme
@@ -25,48 +32,54 @@ fun LogItem(
     type: String,
     content: String,
 ) {
-    val bgColor = when(type){
+    val bgColor = when (type) {
         "ERROR" -> MaterialTheme.colorScheme.errorContainer
         "SUCCESS" -> MaterialTheme.colorScheme.tertiaryContainer
         else -> MaterialTheme.colorScheme.secondaryContainer
     }
-    Column (
+    Row(
         modifier = Modifier
             .padding(5.dp)
+            .height(60.dp)
+            .fillMaxWidth()
             .background(
                 color = if (isSystemInDarkTheme()) Color.DarkGray else Color.LightGray,
             )
     ) {
-            Row(
-                modifier = Modifier.background(color = bgColor).fillMaxWidth().wrapContentHeight(),
-            ) {
-                Text(
-                    text = id.toString(),
-                    modifier = Modifier.padding(5.dp),
-                    style = MaterialTheme.typography.bodySmall
-                )
-                Text(
-                    text = type,
-                    modifier = Modifier.padding(5.dp),
-                    style = MaterialTheme.typography.bodySmall
-                )
-                Text(
-                    text = date,
-                    modifier = Modifier.padding(5.dp),
-                    style = MaterialTheme.typography.bodySmall
-                )
-            }
-            Column {
-
-                HorizontalDivider(color = MaterialTheme.colorScheme.background)
-                    Text(
-                        text = content,
-                        modifier = Modifier.padding(5.dp),
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-
-
+        Column(
+            modifier = Modifier
+                .background(color = bgColor)
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = type.substring(0,2),
+                modifier = Modifier.padding(5.dp),
+                style = MaterialTheme.typography.bodySmall,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        VerticalDivider(thickness = 4.dp, color = MaterialTheme.colorScheme.background)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = date,
+                modifier = Modifier.padding(5.dp),
+                style = MaterialTheme.typography.bodySmall
+            )
+            HorizontalDivider(thickness = 2.dp, color = MaterialTheme.colorScheme.background)
+            Text(
+                text = content,
+                modifier = Modifier.padding(5.dp),
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
 
     }
 }
@@ -76,12 +89,26 @@ fun LogItem(
 private fun LogItemPreview() {
     OllamaUITheme {
         Surface {
-            LogItem(
-                id = 1012,
-                date = "2025-03-12",
-                type = "START",
-                content = "Log content"
-            )
+            Column {
+                LogItem(
+                    id = 1012,
+                    date = "2025-03-12",
+                    type = "START",
+                    content = "Short Log content"
+                )
+                LogItem(
+                    id = 1012,
+                    date = "2025-03-12",
+                    type = "SUCCESS",
+                    content = "Long Log content that fit to box!"
+                )
+                LogItem(
+                    id = 1012,
+                    date = "2025-03-12",
+                    type = "ERROR",
+                    content = "Very Long Log content that can't fit in to place holder and some part of it have to be hidden!"
+                )
+            }
         }
     }
 }
